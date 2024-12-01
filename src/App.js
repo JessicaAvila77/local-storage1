9f58d0a485b4551c424d1f0766d0e994f797b3d6
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-11-30 20:19:43 -0600

    Rutas y componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Proveedor from './components/Proveedor'
 
+//formulamos las rutas y el componente que quiero ver
 function App() {
+
+  //puede ser en directorio raiz '/'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path='listaproveedor' element={<Proveedor/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
